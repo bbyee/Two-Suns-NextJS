62d96affad584598680d7ac17e7ac28686e1164e
Brittany Yee <brittanyyee1@gmail.com>
2020-08-04 20:40:30 -0400

    adding files before merge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8e649ac064acf6bde7e5";
+/******/ 	var hotCurrentHash = "febee4e99e18162aa06b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
