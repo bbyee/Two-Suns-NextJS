c47673d425d43561629e8866af5fdcc1bcfb2420
Brittany Yee <brittanyyee1@gmail.com>
2020-08-04 22:49:42 -0400

    Changed CSS for translated section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d0cc685d18315e1d8dde";
+/******/ 	var hotCurrentHash = "7b4eb9233a850d7fd8c5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
